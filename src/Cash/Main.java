c446f061c32cd1dd79d75b2610954a3e72155270
drag13 <nereality13@mail.ru>
2018-07-23 19:45:35 +0300

    test

---
+++ src/Cash/Main.java
@@ -0,0 +1,63 @@
+package Cash;
+
+import java.util.Scanner;
+
+import static Cash.Users.userName;
+
+public class Main {
+    Scanner in = new Scanner(System.in);
+
+    String searchName;
+
+    public static void main(String[] args) {
+        Cash cash = new Cash();
+        Users user = new Users();
+
+        hello(cash, user);
+
+
+    }
+
+    public static void hello(Cash cash, Users user) {
+        int x = 0;
+        Scanner in = new Scanner(System.in);
+        String searchName;
+        System.out.println("Добро пожаловать в базу данных! ");
+        System.out.println("Для добавления пользователя нажмие  - 1 ");
+        System.out.println("Для получения данных из базы нажмите  - 2");
+        System.out.println("Для выхода - 3");
+
+        while (true) {
+            if (x > 0) {
+                System.out.println("Добавление - 1");
+                System.out.println("Поиск - 2");
+                System.out.println("Выход - 3");
+            }
+            int num = in.nextInt();
+            if (num == 1) {
+                user.scan();
+                cash.addUserInWHM(userName, user.age);
+                user.addUserInFile();
+                System.out.println("Новый пользователь успешно добавлен!");
+                cash.getSize();
+            } else if (num == 2) {
+                System.out.println("ВВедите имя пользователя:  ");
+                in.nextLine();
+                searchName = in.nextLine();
+                System.out.println("Поиск  " + searchName);
+                if(cash.searchUserWHM(searchName)){
+                    cash.searchUserWHM(userName);
+                    }else{
+                    user.seaarchUserInFile();
+                }
+                System.out.println();
+                break;
+            } else {
+                x = 0;
+                break;
+            }
+            x++;
+        }
+    }
+
+}
---
+++ src/Cash/Users.java
@@ -0,0 +1,53 @@
+package Cash;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Users {
+    private static final String FILENAME = "C:\\Users\\drag\\Desktop\\SC11\\Cash\\Users.txt";
+    static String userName;
+    String name;
+
+    static int age;
+    Scanner in = new Scanner(System.in);
+
+
+    void scan() {
+        System.out.println("Введите имя: ");
+        userName = in.nextLine();
+        System.out.println("Введите возраст: ");
+        age = in.nextInt();
+        in.nextLine();
+
+    }
+
+    void addUserInFile() {
+
+        File myFile = new File("C:/Users/drag/Desktop/SC11/cash/Users.txt");
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(myFile, true));
+            String lineSeparator = System.getProperty("line.separator");
+            writer.write(userName + lineSeparator);
+            writer.write(age + lineSeparator);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    void seaarchUserInFile(){
+        try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))) {
+            while ((name = br.readLine()) != userName) {
+                
+
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
